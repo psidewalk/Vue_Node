ca800c6a377c02d25bfce48f06bff5c1471bddc6
Paulo Vicente "Sidewalk <pcsidewalk@gmail.com>
2017-06-24 18:10:12 -0300

    Add files via upload

---
+++ Footer.vue
@@ -0,0 +1,17 @@
+<<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <footer>
+                <p>All Servers are managed here (the buttons aren't working yet)</p>
+                <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn btn-default" v-for="i in 5">{{ i }}</button>
+                </div>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
