f97083a2be3b6d0355e6bb2d927ffae3af839bb6
Paulo Vicente "Sidewalk <pcsidewalk@gmail.com>
2017-06-24 18:08:26 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <app-body></app-body>
+        <hr>
+        <app-footer></app-footer>
+     </div>
+</template>
+
+<script>
+    import Header from "./components/Header.vue"
+    import Body   from "./components/Body.vue"
+    import Footer from "./components/Footer.vue"
+    export default  {
+        components: {
+            'app-header': Header,
+            'app-body': Body,
+            'app-footer': Footer 
+        }
+    }
+</script>
+
+<style>
+
+</style>
